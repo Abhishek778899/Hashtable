6cd94c601333cfc12d33ef6a3c07ecc84bd80adc
Abhishek778899 <abhisheksuryawanshi325@gmail.com>
2022-10-02 11:54:06 +0530

    Remove

---
+++ src/main/java/Hashtable/Hashtable/com/Remove.java
@@ -0,0 +1,26 @@
+package Hashtable.Hashtable.com;
+
+public class Remove {
+
+	public static void main(String[] args) {
+		String sentence = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+		MyHashMap<String, Integer> myHashMap = new MyHashMap<>();
+		String[] words = sentence.toLowerCase().split(" ");
+		for (String word : words) {
+			Integer value = myHashMap.get(word);
+			if (value == null) {
+				value = 1;
+			} else {
+				value = value + 1;
+			}
+			myHashMap.add(word, value);
+
+		}
+		System.out.println(myHashMap);
+		int frequency = myHashMap.get("are");
+		System.out.println("Frequency of word(are) is " + frequency);
+
+		myHashMap.delete("avoidable");
+		System.out.println(myHashMap);
+	}
+}
